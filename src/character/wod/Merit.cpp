b7bff75db41ad84f8dfc051e005730edcb6dfaa1
Patrick Gress <patrick.daniel.gress@googlemail.com>
2013-03-22 02:28:51 +0100

    Switched to YAML-CPP 0.5 interface

--- src/character/wod/Merit.cpp
+++ src/character/wod/Merit.cpp
@@ -3,9 +3,9 @@
 
 namespace schizophrenia {
 
-std::vector<Merit> Merit::Prototypes{100}; 
-  
-  
+std::vector<Merit> Merit::Prototypes {100};
+
+
 Merit Merit::createMerit ( const Merit::IDType& id, Merit::ValueType value, const std::string& description ) {
     auto pos = std::find_if ( Merit::Prototypes.begin(), Merit::Prototypes.end(), [&] ( const Merit& m ) -> bool { return m.ID == id; } );
     if ( pos == Merit::Prototypes.end() ) {
@@ -17,77 +17,25 @@ Merit Merit::createMerit ( const Merit::IDType& id, Merit::ValueType value, cons
     return copy;
     }
 
-size_t Merit::loadFromFile ( const std::string& filename ) {
-    size_t entries{0};
-    std::ifstream source{filename.c_str()};
-    if( ! source ) {
-      throw ( std::runtime_error("Could not load merits from file. Unable to open file " + filename));
-    }
-    YAML::Parser parser ( source );
-    YAML::Node   node;
-    while ( parser.GetNextDocument ( node ) ) {
-        std::string id;
-        std::string name;
-        std::string book;
-        std::string page;
-	std::string type;
-	node["id"] >> id;
-	node["name"] >> name;
-	node["type"] >> type;
-	node["book"] >> book;
-	node["page"] >> page;
-	
-        Merit merit ( id,name, type, book,page );
-	auto begin = node["prequisites"].begin();
-	auto end   = node["prequisites"].end();
-        for ( YAML::Iterator i = begin; i != end; ++i ) {
-            Attribute attribute(*i);
-            merit.Attributes.push_back ( attribute );
-            }
-	begin = node["possibleValues"].begin();
-	end   = node["possibleValues"].end();
-        for ( YAML::Iterator i = begin; i != end; ++i ) {
-            Merit::ValueType value;
-	    std::string description;
-	    (*i)["value"] >> value;
-	    (*i)["description"] >> description;
-            merit.PossibleValues.push_back ( std::make_tuple(value,description) );
-            }
-	begin = node["keywords"].begin();
-	end   = node["keywords"].end();
-        for ( YAML::Iterator i = begin; i != end; ++i ) {
-	    std::string keyword;
-	    *i >> keyword;
-            merit.Keywords.push_back ( keyword );
-            }
-        Merit::Prototypes.push_back ( merit );
-	++entries;
-        }
-        return entries;
-    }
-
-
-
-
 
 Merit::PrequisiteIterator Merit::beginPrequisites ( void ) const {
-    return Merit::PrequisiteIterator ([] ( const Attribute& a ) -> bool { return a.getCategory() == "prequisite"; }, this->Attributes.begin(), this->Attributes.end() );
+    return Merit::PrequisiteIterator ( [] ( const Attribute& a ) -> bool { return a.getCategory() == "prequisite"; }, this->Attributes.begin(), this->Attributes.end() );
     }
 
 Merit::PrequisiteIterator Merit::endPrequisites ( void ) const {
-    return Merit::PrequisiteIterator([] ( const Attribute& a ) -> bool { return a.getCategory() == "prequisite"; }, this->Attributes.end(), this->Attributes.end() );
+    return Merit::PrequisiteIterator ( [] ( const Attribute& a ) -> bool { return a.getCategory() == "prequisite"; }, this->Attributes.end(), this->Attributes.end() );
     }
 
 Merit::PossibleValueIterator Merit::beginValues ( void ) const {
-     return this->PossibleValues.begin();
+    return this->PossibleValues.begin();
     }
-    
+
 Merit::PossibleValueIterator Merit::endValues ( void ) const {
     return this->PossibleValues.end();
     }
-    
-    
-    
+
+
+
 bool Merit::operator== ( const Merit& right ) const {
     return ( this->Description == right.Description && *static_cast<const BasicTrait*> ( this ) == static_cast<const BasicTrait> ( right ) );
     }
@@ -101,9 +49,37 @@ Merit::Merit ( const Merit::IDType& id, const std::string& name, const std::stri
 
     }
 
+YAML::Node Merit::encode(void) const {
+  YAML::Node node = static_cast<const BasicTrait*>(this)->encode();
+  node["type"] = this->Type;
+  node["book"] = this->Book;
+  node["page"] = this->Page;
+  YAML::Node possibleValues = node["values"];
+  for(const std::tuple<ValueType,std::string>& val : this->PossibleValues) {
+      YAML::Node entry;
+      entry["value"]       = std::get<0>(val);
+      entry["description"] = std::get<1>(val);
+      possibleValues.push_back(entry);
+  }
+  return node; 
+}
 
-
-
+bool Merit::decode(const YAML::Node& node) {
+  static_cast<BasicTrait*>(this)->decode(node);
+  this->Type = node["type"].as<std::string>();
+  this->Book = node["book"].as<std::string>();
+  this->Page = node["page"].as<std::string>();
+  this->PossibleValues.clear();
+  YAML::Node possibleValues = node["values"];
+  auto end = possibleValues.end();
+  if(possibleValues.IsSequence()) {
+    for(auto it = possibleValues.begin(); it != end; ++it) {
+        const YAML::Node entry = *it;
+         this->PossibleValues.push_back(std::make_tuple(entry["value"].as<ValueType>(),entry["description"].as<std::string>()));
+    }
+  }
+  return true;
+}
 
 
 
