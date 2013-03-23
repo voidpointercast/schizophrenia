fc257e7c9c55cfabfaf423a61d8d12426b7540ca
Patrick Gress <patrick.daniel.gress@googlemail.com>
2013-03-23 02:42:23 +0100

    Added getter / setter

--- src/character/wod/WorldOfDarknessCharacter.hpp
+++ src/character/wod/WorldOfDarknessCharacter.hpp
@@ -8,15 +8,58 @@
 #ifndef WORLDOFDARKNESSCHARACTER_HPP_
 #define WORLDOFDARKNESSCHARACTER_HPP_
 
+#include<type_traits>
 #include<character/Character.hpp>
+#include<character/wod/Merit.hpp>
+#include<character/wod/Skill.hpp>
+#include<character/wod/Attribute.hpp>
 
 namespace schizophrenia {
+namespace wod {
+class WorldOfDarknessCharacter: public schizophrenia::Character {
+    public:
+        typedef typename std::vector<wod::Attribute>::const_iterator ConstAttributeIterator;
+        typedef typename std::vector<wod::Merit>::const_iterator     ConstMeritIterator;
+        typedef typename std::vector<wod::Skill>::const_iterator     ConstSkillIterator;
 
-	class WorldOfDarknessCharacter: public schizophrenia::Character {
-	public:
-		WorldOfDarknessCharacter(void);
-		virtual ~WorldOfDarknessCharacter();
-	};
+        WorldOfDarknessCharacter ( void );
+        virtual ~WorldOfDarknessCharacter();
 
-} /* namespace schizohrenia */
-#endif /* WORLDOFDARKNESSCHARACTER_HPP_ */
+        void addMerit ( const wod::Merit& merit );
+        void addSkill ( const wod::Skill& skill );
+        
+        std::tuple<bool,std::string> setValue(const Skill& skill,          const typename Skill::ValueType& value);
+        std::tuple<bool,std::string> setValue(const Merit& skill,          const typename Merit::ValueType& value);
+        std::tuple<bool,std::string> setValue(const wod::Attribute& skill, const typename wod::Attribute::ValueType& value);
+
+        void removeMerit ( const wod::Merit& merit );
+        void removeMerit ( const wod::Skill& skill );
+
+        ConstAttributeIterator beginAttributes ( void ) const;
+        ConstAttributeIterator endAttributes ( void ) const;
+        ConstMeritIterator     beginMerits ( void ) const;
+        ConstMeritIterator     endMerits ( void ) const;
+        ConstSkillIterator     beginSkills ( void ) const;
+        ConstSkillIterator     endSkills ( void ) const;
+
+        YAML::Node encode ( void ) const;
+        bool decode ( const YAML::Node& node );
+
+
+    protected:
+
+
+
+        std::vector<wod::Attribute> Attributes;
+        std::vector<wod::Merit>     Merits;
+        std::vector<wod::Skill>     Skills;
+
+
+    };
+
+
+
+
+}
+}
+#endif
