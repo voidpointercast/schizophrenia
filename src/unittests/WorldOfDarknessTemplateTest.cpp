201c29dbfec559b5a716ac52ad44351c5ad2e41a
Patrick Gress <patrick.daniel.gress@googlemail.com>
2013-03-23 02:45:20 +0100

    Changed test to new interface. Added tests.

--- src/unittests/WorldOfDarknessTemplateTest.cpp
+++ src/unittests/WorldOfDarknessTemplateTest.cpp
@@ -3,22 +3,40 @@
 #include<character/wod/WorldOfDarknessCharacter.hpp>
 #include<unittests/Unittest.hpp>
 using namespace schizophrenia;
+using namespace schizophrenia::wod;
 using namespace unittest;
 
-int main(void) {
-	beginTest("WorldOfDarknessCharacter default constructor test");
-	WorldOfDarknessCharacter testchar;
-	endTest(true);
-	beginTest("WorldOfDarknessCharacter add Trait test");
-	testchar.addTrait(BasicTrait("Strength","5"));
-	auto trait = testchar.getTraits().back();
-	endTest(trait.Value == "5" && trait.Name == "Strength");
-	beginTest("PowerStatCheck raise value reject test");
-	endTest(! std::get<0>(testchar.setValue(trait,"6")));
-	beginTest("PowerStatCheck add trait reject test");
-	endTest(! std::get<0>(testchar.addTrait(BasicTrait("Dexterity","6"))));
-	beginTest("PowerStatCheck add second powerstat reject test");
-	testchar.addTrait(BasicTrait("powerstat","4"));
-	endTest(! std::get<0>(testchar.addTrait(BasicTrait("powerstat","5"))));
-	return 0;
-}
+int main ( void ) {
+    beginTest ( "WorldOfDarknessCharacter default constructor test" );
+    WorldOfDarknessCharacter testchar;
+    endTest ( true );
+    beginTest ( "WorldOfDarknessCharacter add Trait test" );
+    testchar.addTrait ( BasicTrait ( "Strength","5" ) );
+    auto trait = testchar.getTraits().back();
+    endTest ( trait.Value == "5" && trait.Name == "Strength" );
+    beginTest ( "PowerStatCheck raise value reject test" );
+    endTest ( ! std::get<0> ( static_cast<Character>(testchar).setValue ( trait,"6" ) ) );
+    beginTest ( "PowerStatCheck add trait reject test" );
+    endTest ( ! std::get<0> ( testchar.addTrait ( BasicTrait ( "Dexterity","6" ) ) ) );
+    beginTest ( "PowerStatCheck add second powerstat reject test" );
+    testchar.addTrait ( BasicTrait ( "powerstat","4" ) );
+    endTest ( ! std::get<0> ( testchar.addTrait ( BasicTrait ( "powerstat","5" ) ) ) );
+    beginTest ( "setWODValue<Skill> test on nonexisting skill" );
+    endTest ( !std::get<0> ( testchar.setValue ( Skill ( "nonexisting",0 ),5 )),"could set value of nonexisting skill" );
+              beginTest ( "addSkill test" );
+              Skill skill ( "skill",5u );
+              testchar.addSkill ( skill );
+              endTest ( true );
+              beginTest ( "setWODValue<Skill> test" );
+    if ( !std::get<0> ( testchar.setValue ( skill,4 ) ) ) {
+    endTest ( false );
+        }
+    auto search = std::find ( testchar.beginSkills(),testchar.endSkills(),skill );
+    if ( search == testchar.endSkills() ) {
+    endTest ( false,"Could not find skill" );
+        }
+    else {
+        endTest ( search->Value == "4","value mismatch" );
+        }
+    return 0;
+    }
