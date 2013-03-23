536879e954fff3fc4183302b761cdc4c5ff745e8
Patrick Gress <patrick.daniel.gress@googlemail.com>
2013-03-23 02:34:43 +0100

    Declares some wod related constant attributes.

---
+++ src/character/wod/Constants.hpp
@@ -0,0 +1,17 @@
+#ifndef SCHIZOPHRENIA_CHARACTER_WOD_CONSTANTS_HPP
+#define SCHIZOPHRENIA_CHARACTER_WOD_CONSTANTS_HPP
+#include<character/Attribute.hpp>
+namespace schizophrenia {
+namespace wod {
+
+  static const schizophrenia::Attribute TenAgain;
+  static const schizophrenia::Attribute NineAgain;
+  static const schizophrenia::Attribute EightAgain;
+  static const schizophrenia::Attribute OneAntiSuccess;
+  static const schizophrenia::Attribute RoteAction;
+  
+  
+  
+}
+}
+#endif
