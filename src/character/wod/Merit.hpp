d7667230a658608e89e35281aefa431768964f83
Patrick Gress <patrick.daniel.gress@googlemail.com>
2013-05-20 03:30:02 +0200

    Added file loading and deserialization methods.

--- src/character/wod/Merit.hpp
+++ src/character/wod/Merit.hpp
@@ -10,39 +10,62 @@ class Merit : public BasicTrait {
         typedef std::string    IDType;
         typedef unsigned short ValueType;
         typedef boost::filter_iterator<std::function<bool ( Attribute ) >,typename std::vector<Attribute>::const_iterator> PrequisiteIterator;
-	typedef typename std::vector<std::tuple<ValueType,std::string>>::const_iterator PossibleValueIterator;
-	
+        typedef typename std::vector<std::tuple<ValueType,std::string>>::const_iterator PossibleValueIterator;
+
         static Merit createMerit ( const IDType& id, ValueType value, const std::string& description = "" );
-	
-	Merit(void) = default;
+
+        template<class OutputIterator>
+        static void loadFromFile(const std::string& filename, OutputIterator out);
+        
+        template<class InputIterator, class OutputIterator>
+        static void deserialize(InputIterator begin, InputIterator end, OutputIterator out);
+        
+        Merit ( void ) = default;
 
         PrequisiteIterator beginPrequisites ( void ) const;
         PrequisiteIterator endPrequisites ( void ) const;
-	PossibleValueIterator beginValues(void) const;
-	PossibleValueIterator endValues(void) const;
+        PossibleValueIterator beginValues ( void ) const;
+        PossibleValueIterator endValues ( void ) const;
 
-        YAML::Node encode(void) const;
-        bool decode(const YAML::Node& node);
+        YAML::Node encode ( void ) const;
+        bool decode ( const YAML::Node& node );
 
         bool operator== ( const Merit& right ) const;
 
         std::string Description;
-	std::string Type;
-	std::string Book;
-	std::string Page;
+        std::string Type;
+        std::string Book;
+        std::string Page;
 
 
     protected:
-	Merit(const IDType& id, const std::string& name, const std::string& type, const std::string& book, const std::string& page);
+        Merit ( const IDType& id, const std::string& name, const std::string& type, const std::string& book, const std::string& page );
 
 
 
     private:
         static std::vector<Merit> Prototypes;
-	std::vector<std::string> Keywords;
-	std::vector<std::tuple<ValueType, std::string>> PossibleValues;
+        std::vector<std::string> Keywords;
+        std::vector<std::tuple<ValueType, std::string>> PossibleValues;
     };
 
+    template<class OutputIterator>
+    void Merit::loadFromFile(const std::string& filename, OutputIterator out) {
+      std::vector<YAML::Node> root = YAML::LoadAllFromFile(filename);
+      auto begin = root.begin();
+      auto end   = root.end();
+      Merit::deserialize(begin,end,out);
+    }
+
+    
+    
+    template<class InputIterator, class OutputIterator>
+    void Merit::deserialize(InputIterator begin, InputIterator end, OutputIterator out) {
+          std::transform(begin,end,out,[&](const typename InputIterator::value_type& e)-> Merit { Merit m; m.decode(e); return m;} );
+    }
+    
+    
+    
 }
 }
 namespace YAML {
