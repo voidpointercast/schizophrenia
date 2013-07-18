f3621d62fc623e4b1ce56ee3c9e0352c2d9a66f1
Patrick Gress <patrick.daniel.gress@googlemail.com>
2013-07-18 22:44:57 +0200

    Added constructors.

--- src/character/wod/Attribute.hpp
+++ src/character/wod/Attribute.hpp
@@ -4,8 +4,37 @@
 namespace schizophrenia {
 namespace wod {
 class Attribute : public BasicTrait {
+    public:
+        typedef unsigned short ValueType;
+        Attribute ( void ) = default;
+        Attribute ( const std::string& name, const ValueType& value );
+        Attribute ( const std::string& id, const std::string& name, const ValueType& value );
+        Attribute ( const std::string& id, const std::string& name, const ValueType& value, const std::string& category );
+        template<class InputIterator>
+        Attribute ( const std::string& name, const ValueType& value, InputIterator begin, InputIterator end );
+        template<class InputIterator>
+        Attribute ( const std::string& id, const std::string& name, const ValueType& value, InputIterator begin, InputIterator end );
+
+
 
     };
+
+template<class InputIterator>
+wod::Attribute::Attribute ( const std::string& name, const Attribute::ValueType& value, InputIterator begin, InputIterator end )
+    : BasicTrait ( name,boost::lexical_cast<std::string>(value),begin,end ) {
+
+    }
+
+template<class InputIterator>
+wod::Attribute::Attribute ( const std::string& id, const std::string& name, const Attribute::ValueType& value, InputIterator begin, InputIterator end )
+    : BasicTrait ( id,name,boost::lexical_cast<std::string>(value),begin,end ) {
+
+    }
+
+
+
+
+
 }
 }
 
