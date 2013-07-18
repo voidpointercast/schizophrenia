c091032a3f1bcbc49f82aafe71b5f77805f1cf58
Patrick Gress <patrick.daniel.gress@googlemail.com>
2013-07-18 22:58:38 +0200

    Added unittest

--- src/unittests/MeritTest.cpp
+++ src/unittests/MeritTest.cpp
@@ -5,6 +5,8 @@ int main(void) {
   unittest::beginTest("Bidirectional Merit to YAML conversion test");
   schizophrenia::wod::Merit merit;
   schizophrenia::wod::Merit copy;
+  merit.Name  = "NAME";
+  merit.Book  = "BOOK";
   merit.Value = 5;
   try {
     copy.decode(merit.encode());
@@ -15,17 +17,11 @@ int main(void) {
   unittest::endTest(copy == merit, "Copy and original do not match");
   
   
-  unittest::beginTest("Load merits from YAML file basic test");
-  
-  try {
-    YAML::Node doc = YAML::LoadFile("./data/testdata/merittest.yaml");
-    merit.decode(doc);
-  } catch(YAML::Exception& error) {
-    unittest::endTest(false,"Could not parse merit file correctly: " + std::string(error.what()));
-    return 1;
-  }
-  unittest::endTest(true);
   unittest::beginTest("Load merits from YAML file data integration test");
-  unittest::endTest(merit.Name == "NAME" && merit.Book == "BOOK" && std::get<0>(*merit.beginValues()) == 1);
+  unittest::endTest(merit.Name == "NAME" && merit.Book == "BOOK" && std::distance(merit.beginValues(),merit.endValues())==0);
+  unittest::beginTest("Load merits from file using LoadFromFile method");
+  std::vector<schizophrenia::wod::Merit> merits;
+  schizophrenia::wod::Merit::loadFromFile("./data/testdata/merits.yaml",std::back_inserter(merits));
+  unittest::endTest(true);
   return 0;
 }
