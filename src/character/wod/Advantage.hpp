420c52db7eaae27224ae00f74b2035215b45ebe6
Patrick Gress <patrick.daniel.gress@googlemail.com>
2013-08-07 20:25:19 +0200

    Initial commit

---
+++ src/character/wod/Advantage.hpp
@@ -0,0 +1,48 @@
+#ifndef CHARACTER_WOD_ADVANTAGE_HPP
+#define CHARACTER_WOD_ADVANTAGE_HPP
+#include <character/BasicTrait.hpp>
+
+namespace schizophrenia {
+namespace wod {
+
+class Advantage : public BasicTrait {
+public:
+
+    static const std::string Category;
+
+    Advantage ( void ) = default;
+
+    virtual ~Advantage(void);
+
+    Advantage (const std::string& id, const std::string& name, const std::string& value );
+
+    bool operator==(const Advantage& other) const;
+
+    bool IsDisadvantage;
+    
+    YAML::Node encode(void) const;
+    
+    bool decode(const YAML::Node& node);
+
+
+};
+
+
+}
+}
+
+namespace YAML {
+template<>
+struct convert<schizophrenia::wod::Advantage> {
+    static Node encode ( const schizophrenia::wod::Advantage& advantage ) {
+        return advantage.encode();
+    }
+
+    static bool decode ( const Node& node, schizophrenia::wod::Advantage& advantage ) {
+        return advantage.decode ( node );
+    }
+};
+}
+
+
+#endif
